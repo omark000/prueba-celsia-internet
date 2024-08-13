8ee1cdc88b196e65c3662e903f0f561cfcc46248
ozambrano <omar.zambrano@iptotal.com>
2024-08-13 08:01:52 -0500

    commit inicial

---
+++ src/main/java/com/celsia/testCelsia/TestCelsiaApplication.java
@@ -0,0 +1,13 @@
+package com.celsia.testCelsia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCelsiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCelsiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celsia/testCelsia/TestCelsiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celsia.testCelsia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCelsiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
